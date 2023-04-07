60a340281d58b4e3397eb360581c9874cfc67b8c
SahilMangla14 <108575832+SahilMangla14@users.noreply.github.com>
2023-04-07 19:36:10 +0530

    minor changes

--- src/app.py
+++ src/app.py
@@ -7,15 +7,19 @@ CORS(app)
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
-    content = file.read()
+    content = file.read().decode('utf-8')
+    
     prog_mc_file = request.form.get('prog_mc_file') == 'true'
     pipelining_enabled = request.form.get('pipelining_enabled') == 'true'
     forwarding_enabled = request.form.get('forwarding_enabled') == 'true'
     print_registers_each_cycle = request.form.get('print_registers_each_cycle') == 'true'
     print_pipeline_registers = request.form.get('print_pipeline_registers') == 'true'
     print_specific_pipeline_registers = request.form.get('print_specific_pipeline_registers') == 'true'
-    # print(pipelining_enabled)
+
     # Do something with the file and the boolean values
+    demo = open('demofile.txt','w')
+    demo.write(content)
+    demo.close()
     f  = open("input.txt",'w')
     f.close()
     f = open('input.txt','a')
